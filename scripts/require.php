484748cc958154d222441e3751aefe8b9e810603
Asao <kamei@workspot.jp>
2013-02-17 17:22:46 +0900

    adds container to require.

--- scripts/require.php
+++ scripts/require.php
@@ -4,3 +4,4 @@ require( __DIR__ . '/../src/WScore/DiContainer/Utils.php' );
 require( __DIR__ . '/../src/WScore/DiContainer/Parser.php' );
 require( __DIR__ . '/../src/WScore/DiContainer/Analyzer.php' );
 require( __DIR__ . '/../src/WScore/DiContainer/Forger.php' );
+require( __DIR__ . '/../src/WScore/DiContainer/Container.php' );
