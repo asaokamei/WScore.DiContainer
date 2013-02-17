f127876a12146c23388941f951e50030399d5e7c
Asao <kamei@workspot.jp>
2013-02-17 16:24:55 +0900

    Y should test injecting property defined in parent class.

--- tests/WScore/DiContainer/MockClass/Y.php
+++ tests/WScore/DiContainer/MockClass/Y.php
@@ -3,9 +3,6 @@ namespace WScore\tests\DiContainer\MockClass;
 
 class Y extends X
 {
-    /** @var \WScore\tests\DiContainer\MockClass\C  */
-    public $setC;
-
     /**
      * @Inject
      * @param \WScore\tests\DiContainer\MockClass\CC $c
