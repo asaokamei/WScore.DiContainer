f077d59babb266837b174d2371a8e2624bff5df0
asao Kamei <asao@ubuPC.(none)>
2013-02-18 11:11:41 +0900

    refactored to put option stuff into a private method; added comments.

--- src/WScore/DiContainer/Container.php
+++ src/WScore/DiContainer/Container.php
@@ -18,35 +18,50 @@ class Container implements ContainerInterface
         $this->forger = $forger;
     }
 
+    /**
+     * @param string $id
+     * @param mixed  $value
+     * @param null $option
+     */
     public function set( $id, $value, $option = null ) {
         $this->value[ $id ] = $value;
         if( isset( $option ) ) $this->setOption( $id, $option );
     }
 
+    /**
+     * @param string $id
+     * @param array  $option
+     */
     public function setOption( $id, $option ) {
         $this->option[ $id ] = Utils::normalizeInjection( $option );
     }
 
+    /**
+     * @param string $id
+     * @return bool
+     */
     public function exists( $id ) {
         return array_key_exists( $id, $this->value );
     }
 
+    /**
+     * @param string $id
+     * @param array  $option
+     * @return mixed|void
+     */
     public function get( $id, $option = array() )
     {
         $found = $id;
-        if( array_key_exists( $id, $this->value ) ) $found = $this->value[ $id ];
+        if( array_key_exists( $id, $this->value ) ) {
+            $found = $this->value[ $id ];  // set found value.
+        }
         // check if $found is a closure, or a className to construct.
         if( $found instanceof \Closure ) {
             $found = $found( $this );
         }
         elseif( Utils::isClassName( $found ) ) {
-            // prepare options
-            $option = Utils::normalizeOption( $option ); // normalize input option
-            if( isset( $this->option[$id] ) ) { 
-                $option = Utils::mergeOption( $this->option[$id], $option );
-            }
-            $inject = $this->forger->analyze( $found );
-            $option = Utils::mergeOption( $inject, $option );
+            // it's a class. prepare options to construct an object.
+            $option = $this->prepareOption( $id, $found, $option );
             $found  = $this->forger->forge( $this, $found, $option );
             // singleton: store found object into cached.
             if( array_key_exists( 'singleton', $option ) && $option[ 'singleton' ] ) {
@@ -54,6 +69,23 @@ class Container implements ContainerInterface
             }
         }
         return $found;
-        
+    }
+
+    /**
+     * @param string $id
+     * @param string $className
+     * @param array  $option
+     * @return array
+     */
+    private function prepareOption( $id, $className, $option )
+    {
+        $option = Utils::normalizeOption( $option ); // normalize input option
+        if( isset( $this->option[$id] ) ) {
+            // get pre-set option from $option, and merge it with the given option.
+            $option = Utils::mergeOption( $this->option[$id], $option );
+        }
+        $inject = $this->forger->analyze( $className );
+        $option = Utils::mergeOption( $inject, $option );
+        return $option;
     }
 }
