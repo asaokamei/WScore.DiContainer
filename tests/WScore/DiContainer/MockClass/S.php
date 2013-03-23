e74cf01adf78bbd8aa929b9866e126081a54b62b
Asao <kamei@workspot.jp>
2013-03-23 12:33:28 +0900

    added mock class, S, for testing singleton.

---
+++ tests/WScore/DiContainer/MockClass/S.php
@@ -0,0 +1,14 @@
+<?php
+namespace WScore\tests\DiContainer\MockClass;
+
+/**
+ * Class S
+ *
+ * @package WScore\tests\DiContainer\MockClass
+ * 
+ * @singleton
+ */
+class S
+{
+    public $name = 'S class';
+}
--- tests/WScore/DiContainer/MockClass/require.php
+++ tests/WScore/DiContainer/MockClass/require.php
@@ -3,6 +3,7 @@ require( __DIR__ . '/A.php' );
 require( __DIR__ . '/B.php' );
 require( __DIR__ . '/C.php' );
 require( __DIR__ . '/CC.php' );
+require( __DIR__ . '/S.php' );
 require( __DIR__ . '/X.php' );
 require( __DIR__ . '/Y.php' );
 require( __DIR__ . '/Container.php' );
